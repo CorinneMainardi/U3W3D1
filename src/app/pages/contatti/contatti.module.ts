00c3d352a1aaa9cc337ff6a5d3af576d6f9db7cf
CorinneMainardi <147351705+CorinneMainardi@users.noreply.github.com>
2024-10-21 15:23:10 +0200

    creazione moduli

---
+++ src/app/pages/contatti/contatti.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ContattiComponent } from './contatti.component';
+
+
+
+@NgModule({
+  declarations: [
+    ContattiComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ContattiModule { }

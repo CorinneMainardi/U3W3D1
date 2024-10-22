7772116e52008dd276c460b8bdc2650650086eb7
CorinneMainardi <147351705+CorinneMainardi@users.noreply.github.com>
2024-10-22 15:11:19 +0200

    modifiche

--- src/app/pages/contatti/contatti.component.ts
+++ src/app/pages/contatti/contatti.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-contatti',
   templateUrl: './contatti.component.html',
-  styleUrl: './contatti.component.scss'
+  styleUrl: './contatti.component.scss',
 })
-export class ContattiComponent {
-
-}
+export class ContattiComponent {}

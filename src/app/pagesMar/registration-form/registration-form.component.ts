30ce98742c299b2ff42a8199316cfdc686a16b9d
CorinneMainardi <147351705+CorinneMainardi@users.noreply.github.com>
2024-10-22 17:04:50 +0200

    commit

--- src/app/pagesMar/registration-form/registration-form.component.ts
+++ src/app/pagesMar/registration-form/registration-form.component.ts
@@ -29,7 +29,7 @@ export class RegistrationFormComponent {
       }),
     });
   }
-  send() {
+  sendForm() {
     this.getMessage('passwordData');
   }
   correctPassword = (formC: FormControl): ValidationErrors | null => {
